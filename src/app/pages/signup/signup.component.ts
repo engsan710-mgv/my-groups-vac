8e1e29fbb795f3aafcc3fef75b8ecaf08fe537a6
engsan710@gmail.com <engsan710@gmail.com>
2020-01-04 19:28:07 -0500

    Adding signup code

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+  
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login(): void {
+    //TODO
+    //Add google login here
+    if (this.email) {
+      this.router.navigate(["user"]);
+    } else {
+      alert("Invalid credentials");
+    }
   }
 
 }
